d501c636fc5f29bc34664254c8bb350532a3fa4e
Aryabardhansinghchauhan <aryabardhansinghchauhan@gmail.com>
2026-08-21 16:46:03 +0530

    Update CORS configuration for the application

--- backend/index.js
+++ backend/index.js
@@ -8,14 +8,17 @@ import cookieParser from 'cookie-parser'
 import userRouter from './routes/user.routes.js'
 
 
-const app = express()
-app.use(cors(
-    {
-        origin:"http://localhost:5173",
-        credentials:true
-    }
+const app = express();
 
-))
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://virtual-assistant-rose.vercel.app"
+    ],
+    credentials: true
+  })
+)
 const port =process.env.PORT || 5000
 app.use(express.json())
 app.use(cookieParser())
